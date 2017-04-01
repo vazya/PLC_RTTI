09ecba2975f45e0a019c95574462b82b4561514c
Зятчин Валентин Михайлович <muxajlbl4.08@mail.ru>
2017-04-02 01:13:48 +0300

    добавил обработку исключения

--- PLC_RTTI/main.cpp
+++ PLC_RTTI/main.cpp
@@ -81,52 +81,53 @@ void withoutRTTI()
 	A* c = dynamic_cast< A* >( new A() );
 	if( c ) {
 		cout << "A* c = dynamic_cast<A*>( new A() ) succeded \n";
+		cout << "c name : " << typeid( c ).name() << endl;
+		cout << "c hash : " << typeid( c ).hash_code() << endl << endl;
+		delete c;
 	} else {
 		cout << "A* c = dynamic_cast<A*>( new A() ) failed \n";
 	}
-	cout << "c name : " << typeid( c ).name() << endl;
-	cout << "c hash : " << typeid( c ).hash_code() << endl << endl;
-	delete c;
 
 	A* d = dynamic_cast< A* >( new B() );
 	if( d ) {
 		cout << "A* d = dynamic_cast<A*>( new B() ) succeded \n";
+		cout << "d name : " << typeid( d ).name() << endl;
+		cout << "d hash : " << typeid( d ).hash_code() << endl << endl;
+		delete d;
 	} else {
 		cout << "A* d = dynamic_cast<A*>( new B() ) failed \n";
 	}
-	cout << "d name : " << typeid( d ).name() << endl;
-	cout << "d hash : " << typeid( d ).hash_code() << endl << endl;
-	delete d;
 
 	B* e = dynamic_cast< B* >( new B() );
 	if( e ) {
 		cout << "B* e = dynamic_cast<B*>( new B() ) succeded \n";
+		cout << "e name : " << typeid( e ).name() << endl;
+		cout << "e hash : " << typeid( e ).hash_code() << endl << endl;
+		delete e;
 	} else {
 		cout << "B* e = dynamic_cast<B*>( new B() ) failed \n";
 	}
-	cout << "e name : " << typeid( e ).name() << endl;
-	cout << "e hash : " << typeid( e ).hash_code() << endl << endl;
-	delete e;
 
-	//try {
-	//	B* f = dynamic_cast< B* >( new A() );
-	//	if( f ) {
-	//		cout << "B* f = dynamic_cast<B*>( new A() ) succeded \n";
-	//	} else {
-	//		cout << "B* f = dynamic_cast<B*>( new A() ) failed \n";
-	//	}
-	//	cout << "f name : " << typeid( f ).name() << endl;
-	//	cout << "f hash : " << typeid( f ).hash_code() << endl << endl;
-	//	delete f;
-	//}
-	//catch( std::exception& e ) {
-	//	cout << e.what() << endl;
-	//}
+	B* f( 0 );
+	try {
+		f = dynamic_cast< B* >( new A() );
+	} 
+	catch( exception& e ) {
+		cout << "Exception: " << e.what() << endl;
+	}
+	if( f ) {
+		cout << "B* f = dynamic_cast<B*>( new A() ) succeded \n";
+		cout << "f name : " << typeid( f ).name() << endl;
+		cout << "f hash : " << typeid( f ).hash_code() << endl << endl;
+		delete f;
+	} else {
+		cout << "B* f = dynamic_cast<B*>( new A() ) failed \n";
+	}
 }
 
 int main()
 {
-	fun();
+	//fun();
 	//withRTTI();
 	withoutRTTI();
 	system( "pause" );
